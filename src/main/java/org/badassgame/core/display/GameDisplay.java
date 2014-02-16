2c4941b49e54b67c4423ae6bc8cd6cf6903c7a49
Alexander Richard Smith <alexander.richard.smith@gmail.com>
2014-02-16 02:46:30 +0000

    Added Goodinstance, GameView, Main

---
+++ src/main/java/org/badassgame/core/display/GameDisplay.java
@@ -0,0 +1,11 @@
+package org.badassgame.core.display;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Alex
+ * Date: 2/15/14
+ * Time: 8:19 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public interface GameDisplay {
+}
---
+++ src/main/java/org/badassgame/core/main/Main.java
@@ -0,0 +1,11 @@
+package org.badassgame.core.main;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Alex
+ * Date: 2/15/14
+ * Time: 9:42 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Main {
+}

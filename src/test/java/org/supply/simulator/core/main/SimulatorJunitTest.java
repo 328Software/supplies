b51baf6b4133a9514b6d9291b9cab344c3323cbe
Brandon Charles Estle <bcestle@gmail.com>
2014-06-01 19:46:02 +0000

    Created some simple files to test the simulator setup.

---
+++ src/test/java/org/supply/simulator/core/main/SimulatorJunitTest.java
@@ -0,0 +1,15 @@
+package org.supply.simulator.core.main;
+
+import org.junit.Test;
+
+/**
+ * Created by Brandon on 6/1/2014.
+ */
+public class SimulatorJunitTest {
+
+
+    @Test
+    public void test() {
+        System.out.println("it's working!!");
+    }
+}
